d2887f9a1faa4cdeca4eb9e2459737677b426297
maheensohail1 <maheensohail2003@gmail.com>
2024-06-07 17:14:50 +0000

    comment added

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import coursesRouter from './routes/courses-routes';
 
-
+//comment added to check CI/CD
 const PORT = 5000;
 const app = express();
 
